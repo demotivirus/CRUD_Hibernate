134469b2cc2f247add79601f5342f97386021031
demotivirus <demotivirus@gmail.com>
2020-04-13 16:02:02 +0300

    Add allUsers

--- src/main/java/servlet/ListUsersServlet.java
+++ src/main/java/servlet/ListUsersServlet.java
@@ -1,6 +1,5 @@
 package servlet;
-import com.google.gson.Gson;
-import model.User;
+
 import service.UserService;
 
 import javax.servlet.ServletException;
@@ -9,18 +8,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
-@WebServlet("/allUsers")
+@WebServlet("/showAllUsers")
 public class ListUsersServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Gson gson = new Gson();
-
-        resp.setContentType("application/json");
-        resp.setCharacterEncoding("UTF-8");
-        String json = gson.toJson(UserService.getInstance().getAll());
-        resp.getWriter().println(json);
+        req.setAttribute("listUser", UserService.getInstance().getAll());
         resp.setStatus(200);
+        req.getRequestDispatcher("allUsers.jsp").forward(req, resp);
     }
 }
